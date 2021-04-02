7daffda21dcc0a4b478a47eeab8bab244444ed90
MetalWing <deniskha@gmail.com>
2021-04-01 22:31:36 -0400

    Join game textbox added with transitions

--- src/components/home.js
+++ src/components/home.js
@@ -1,60 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-import { Button, Grid, Container } from '@material-ui/core';
+import { Button, Grid, Container, TextField } from '@material-ui/core';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Fade from '@material-ui/core/Fade';
+
+
 import PersonIcon from '@material-ui/icons/Person';
 import PeopleIcon from '@material-ui/icons/People';
 import HelpIcon from '@material-ui/icons/Help';
 
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 
 export default function Home()  {
+    const [join, joinClicked] = useState(false);
     const history = useHistory();
-
     const routeChange = (newPath) =>{
         history.push(newPath);
     }
+    const [checked, setChecked] = React.useState(false);
+
+    const handleJoinClicked = () => {
+      joinClicked((prev) => !prev);
+    };
     return (
         <Container maxWidth="md">
                 <Grid 
                 container
                 spacing={1}
                 alignItems="center"
+                direction="column"
                 justify="center"
                 style={{ minHeight: '100vh', flexWrap: 'nowrap' }}>
-                    <Grid item xs={12}>
+                    <Grid item xs={12} style={{width: "100%"}}>
                         <Button 
                         color="primary" 
                         fullWidth={true} 
                         variant="outlined"
                         onClick={() => routeChange('lobby')}>
                             <Grid>
-                                <PersonIcon /><br />Host
+                                <PersonIcon style={{ fontSize: 40 }} /><br />Host
                             </Grid>
                         </Button>
                     </Grid>
-                    <Grid item xs={12}>
-                        <Button
+                    <Grid item xs={12} style={{width: "100%"}}>
+                        {!join && <Button
                         color="primary" 
-                        fullWidth={true} 
-                        variant="outlined">
+                        fullWidth={true}
+                        variant="outlined"
+                        onClick={handleJoinClicked}>
                             <Grid>
-                                <PeopleIcon /><br />Join
+                                <PeopleIcon style={{ fontSize: 40 }} /><br />Join
                             </Grid>
-                        </Button>
+                        </Button>}
+                        <Fade in={join} timeout={{ enter: 1000 }} unmountOnExit>
+                        <TextField id="outlined-basic" variant="outlined" fullWidth={true} InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <PeopleIcon style={{ fontSize: 40 }} />
+                              </InputAdornment>
+                            )
+                          }} /></Fade>
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={12} style={{width: "100%"}}>
                         <Button 
                         color="primary" 
                         fullWidth={true} 
                         variant="outlined" 
                         onClick={() => routeChange('help')}>
                             <Grid>
-                                <HelpIcon /><br />Help
+                                <HelpIcon style={{ fontSize: 40 }} /><br />Help
                             </Grid>
                         </Button>
                     </Grid>
                 </Grid>
         </Container>
     );
+}
+
+function showRoomCodePrompt() {
+    console.log('%c ayy lmao', 'color: hotpink');
 }
--- src/components/lobby.js
+++ src/components/lobby.js
@@ -1,7 +1,37 @@
-import React from "react";
+// import React from "react";
 
-export default class Lobby extends React.Component {
-    render() {
-      return <h1>Hello, {this.props.name}</h1>;
-    }
-  }
+// export default class Lobby extends React.Component {
+//     render() {
+//       return <h1>Hello, {this.props.name}</h1>;
+//     }
+//   }
+
+  import React from 'react';
+import Switch from '@material-ui/core/Switch';
+import Fade from '@material-ui/core/Fade';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { TextField } from '@material-ui/core';
+
+
+
+export default function Lobby() {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
+  return (
+    <div >
+      <div>
+        <FormControlLabel
+          control={<Switch checked={checked} onChange={handleChange} />}
+          label="Show"
+        />
+        <Fade  in={checked}>
+        <TextField id="outlined-basic" size="large" variant="outlined" fullWidth={true} />
+        </Fade>
+      </div>
+    </div>
+  );
+}
